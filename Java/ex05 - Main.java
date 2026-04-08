3c6016d41fbaa8cd8cd457156d713399f2ce0b53
Noemi Contrera <eliziarionoemi@gmail.com>
2026-04-08 17:28:01 -0300

    Delete Java directory

--- Java/ex05 - Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-	public static void main(String [] args) {
-		ControleRemoto controle1 = new ControleRemoto();
-		controle1.ligar();
-		controle1.desligarMudo();
-		controle1.abrirMenu();
-		
-	}
-}
