c8f3d297481f7862f87a7cec1f8f583dc2c65428
Noemi Contrera <eliziarionoemi@gmail.com>
2026-04-23 20:44:47 -0300

    Add console clear before distance output
    Clear the console before printing the distance.

--- Python/a08ex05.py
+++ Python/a08ex05.py
@@ -3,13 +3,11 @@ import os, math
 def distancia2p(xa, ya, xb, yb):
     return math.sqrt((xb-xa)**2 + (yb-ya)**2)
 
-
 xa = float(input("Digite X (1): "))
 ya = float(input("Digite Y (1): "))
 xb = float(input("Digite X (2): "))
 yb = float(input("Digite Y (2): "))
 
+os.system('cls')
 d = distancia2p(xa, xb, ya, yb)
-print(d)
-
-# d= ((x2-x1)**2 + (y2-y1)**2)**1(1/2)
+print("A distância é ", d)
